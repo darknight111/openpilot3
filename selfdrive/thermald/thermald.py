f5ef113b7e050fea8cb7dafa1f7e7efce9cd80de
darknight111 <75706333+darknight111@users.noreply.github.com>
2021-03-14 21:29:06 -0700

    Update thermald.py

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -347,8 +347,7 @@ def thermald_thread():
     startup_conditions["device_temp_good"] = thermal_status < ThermalStatus.danger
     set_offroad_alert_if_changed("Offroad_TemperatureTooHigh", (not startup_conditions["device_temp_good"]))
 
-    startup_conditions["hardware_supported"] = pandaState is not None and pandaState.pandaState.pandaType not in [log.PandaState.PandaType.whitePanda,
-                                                                                                   log.PandaState.PandaType.greyPanda]
+    startup_conditions["hardware_supported"] = True
     set_offroad_alert_if_changed("Offroad_HardwareUnsupported", pandaState is not None and not startup_conditions["hardware_supported"])
 
     # Handle offroad/onroad transition
