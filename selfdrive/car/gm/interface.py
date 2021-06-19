88d5e16bc74334c7aa5ee0d9be8e0611c576d9f9
darknight111 <75706333+darknight111@users.noreply.github.com>
2021-06-19 11:32:44 -0700

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -71,7 +71,7 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = 2.0828 
       #PID tunning not to prevent oversteer
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[10., 25.0], [10., 25.0]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.1, 0.12], [0.006, 0.012]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.108, 0.1296], [0.006, 0.012]]
       ret.lateralTuning.pid.kdBP = [0.]
       ret.lateralTuning.pid.kdV = [0.7]  #corolla from shane fork : 0.725
       ret.lateralTuning.pid.kf = 0.00006
