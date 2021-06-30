61815414b53551378a6733f31b7adaa7ff5b12ad
darknight111 <75706333+darknight111@users.noreply.github.com>
2021-06-29 22:29:32 -0700

    Almost there on tuning! Trying to correct lazy lane centering
    Increased Ki

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -43,7 +43,7 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.00]]
     ret.lateralTuning.pid.kf = 0.00004   # full torque for 20 deg at 80mph means 0.00007818594
     ret.steerRateCost = 0.5
-    ret.steerActuatorDelay = 0.23
+    ret.steerActuatorDelay = 0.2
 
     ret.enableGasInterceptor = 0x201 in fingerprint[0]
     #TODO: this should be case based
@@ -71,7 +71,7 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = 2.0828 
       #PID tunning not to prevent oversteer
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[10., 41.0], [10., 41.0]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18, 0.261], [0.01, 0.0212]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.165, 0.26], [0.01, 0.0223]]
       ret.lateralTuning.pid.kdBP = [10., 41.0]
       ret.lateralTuning.pid.kdV = [0.7, 0.8]  #corolla from shane fork : 0.725
       ret.lateralTuning.pid.kf = 0.00006
