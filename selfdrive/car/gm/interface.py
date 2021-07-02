6f3330eadcbbf4aefaf614d2ace4e62adac3d474
darknight111 <75706333+darknight111@users.noreply.github.com>
2021-07-02 09:07:00 -0700

    Correcting lazy lane centering
    Almost there!

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -71,7 +71,7 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = 2.0828 
       #PID tunning not to prevent oversteer
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[10., 41.0], [10., 41.0]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.155, 0.251], [0.0102, 0.0244]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.154, 0.244], [0.0102, 0.0255]]
       ret.lateralTuning.pid.kdBP = [10., 41.0]
       ret.lateralTuning.pid.kdV = [0.7, 0.8]  #corolla from shane fork : 0.725
       ret.lateralTuning.pid.kf = 0.00006
