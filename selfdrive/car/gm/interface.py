f35dda595e99550421c002cb1f92681a4ae8276e
darknight111 <75706333+darknight111@users.noreply.github.com>
2021-08-24 11:34:46 -0700

    Solid tune improvements

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -43,7 +43,7 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.00]]
     ret.lateralTuning.pid.kf = 0.00004   # full torque for 20 deg at 80mph means 0.00007818594
     ret.steerRateCost = 0.50
-    ret.steerActuatorDelay = 0.
+    ret.steerActuatorDelay = 0.15
 
     ret.enableGasInterceptor = 0x201 in fingerprint[0]
     #TODO: this should be case based
@@ -70,10 +70,10 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = 2.0828 
       tire_stiffness_factor = 1.0
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[10., 41.0], [10., 41.0]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18, 0.26], [0.01, 0.02]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18, 0.26], [0.01, 0.0203]]
       ret.lateralTuning.pid.kdBP = [0.]
-      ret.lateralTuning.pid.kdV = [0.3]  #corolla from shane fork : 0.725
-      ret.lateralTuning.pid.kf = 0.00007
+      ret.lateralTuning.pid.kdV = [0.4]  #corolla from shane fork : 0.725
+      ret.lateralTuning.pid.kf = 0.000075
 
     elif candidate == CAR.MALIBU:
       # supports stop and go, but initial engage must be above 18mph (which include conservatism)
